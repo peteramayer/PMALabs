38ce2cbefba7482cf30c53b1feac3a86a1c7f776
Keegan Brown <brown.keegan@gmail.com>
2014-02-04 17:55:29 +0000

    raslab cleaning and init

---
+++ rasLabExperiments/config.js
@@ -0,0 +1,6 @@
+exports.twitterConfig =	{
+	consumer_key:				'jhsFZIY7u7XwipVRKdeQ',
+	consumer_secret:			'VNLDYCU3KcoVolmiW1OaKHvDZjXusQ6o9am4eyOo4',
+	access_token_key:			'2295065413-PIXshQjUdsABuLc2WPFVceP14tG6HceMk4gtlPm',
+	access_token_secret: 		'IPXrwTvPG7e70bmkAHDuoaNW1TYjsqSleft8JSA8a51Sx'
+}
